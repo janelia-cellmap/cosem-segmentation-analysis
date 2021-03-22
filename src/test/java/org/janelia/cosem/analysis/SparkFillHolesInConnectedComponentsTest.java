8d588ef227396626c7e2b3b2ec6d4d055bad54f5
David Ackerman <ackermand@janelia.hhmi.org>
2021-03-22 11:16:37 -0400

    moved helper files to their own directory

---
+++ src/test/java/org/janelia/cosem/analysis/SparkFillHolesInConnectedComponentsTest.java
@@ -0,0 +1,18 @@
+package org.janelia.cosem.analysis;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import org.apache.spark.SparkConf;
+import org.janelia.cosem.analysis.SparkFillHolesInConnectedComponents;
+import org.junit.Test;
+
+public class SparkFillHolesInConnectedComponentsTest {
+    
+    @Test
+    public void testConnectedComponents() throws Exception {
+	SparkFillHolesInConnectedComponents.setupSparkAndFillHolesInConnectedComponents(TestHelper.testN5Locations, "shapes_cc", 0, "_filled", false, false);
+	assertTrue(TestHelper.validationAndTestN5sAreEqual("shapes_cc_filled"));
+    }
+
+}
